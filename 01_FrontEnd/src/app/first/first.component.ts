3d6d5ccdc65b05310fc87c01c1d9a34492d4ff8b
Tajnoor Sabbir <tajnoor.sabbir@northsouth.edu>
2023-06-13 12:05:04 +0600

    jun13 12.05

--- 01_FrontEnd/src/app/first/first.component.ts
+++ 01_FrontEnd/src/app/first/first.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
+
+  // property
+  title = "Angualr Full Course in Bangla";
+
+  // array
+  items: any[] = [
+    'id', 'name', 'age'
+  ]
+
+  constructor() {}
+
+  ngOnInit():void {
+    this.f();
+    this.f2();
+  }
+
+
+  //function -> f(){}
+  f(){
+    console.log("Hello Bangladesh");
+  }
+
+  f2(){
+    return this.title;
+  }
+
+  sum(){
+    return 20+10;
+  }
 
 }
