a0bcb968d96c939bf71406b7ee98c6ec2d84ea14
Tajnoor Sabbir <tajnoor.sabbir@northsouth.edu>
2023-06-15 14:51:26 +0600

    jun15 3pm

--- 04_Form-Validation/src/app/app.component.ts
+++ 04_Form-Validation/src/app/app.component.ts
@@ -112,6 +112,7 @@ export interface User {
   confirmPassword: string;
 }
 
+// Password-ConfirmPassword mismached fn()
 function matchPasswordValidator(passwordKey: string): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const password = control.root.get(passwordKey);
@@ -131,6 +132,22 @@ function matchPasswordValidator(passwordKey: string): ValidatorFn {
 export class AppComponent implements OnInit {
   user: User;
 
+
+  //User name Validators for "First Name" & "Last Name"
+  nameValidators = [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(30)
+  ];
+
+  //Password Validators for "Password" & "Confirm Password"
+  passwordValidators = [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(30),
+    Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+  ];
+
   constructor() {
     this.user = {} as User;
     this.myForm = new FormGroup({
@@ -145,24 +162,15 @@ export class AppComponent implements OnInit {
     });
   }
 
+
   ngOnInit(): void {}
 
-  nameValidators = [
-    Validators.required,
-    Validators.minLength(3),
-    Validators.maxLength(30)
-  ];
-
-  passwordValidators = [
-    Validators.required,
-    Validators.minLength(8),
-    Validators.maxLength(30),
-    Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
-  ];
-
+  
   // Form
   myForm: FormGroup;
 
+
+  //get method
   get firstName() {
     return this.myForm.get('firstName');
   }
@@ -179,7 +187,113 @@ export class AppComponent implements OnInit {
     return this.myForm.get('confirmPassword');
   }
 
+  //for Submit button
   onSubmit() {
     console.log(this.myForm.value);
   }
 }
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormControl, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+
+// export interface User {
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   password: string;
+//   confirmPassword: string;
+// }
+
+
+// // Password-ConfirmPassword mismached fn()
+// function matchPasswordValidator(passwordKey: string): ValidatorFn {
+//   return (control: AbstractControl): { [key: string]: any } | null => {
+//     const password = control.root.get(passwordKey);
+//     const confirmPassword = control.value;
+//     if (password && confirmPassword !== password.value) {
+//       return { 'passwordMismatch': true };
+//     }
+//     return null;
+//   };
+// }
+
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements OnInit {
+  
+  
+// //user initial defined
+//   user = {
+//         firstName: '',
+//         lastName: '',
+//         email: '',
+//         password: '',
+//         confirmPassword: ''
+//       }
+
+//   constructor() {
+//     this.user = {} as User;
+    
+//   }
+
+//   ngOnInit(): void {}
+
+//   nameValidators = [
+//     Validators.required,
+//     Validators.minLength(3),
+//     Validators.maxLength(30)
+//   ];
+
+//   passwordValidators = [
+//     Validators.required,
+//     Validators.minLength(8),
+//     Validators.maxLength(30),
+//     Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+//   ];
+
+//   // Form
+//   myForm = new FormGroup({
+//     firstName: new FormControl(this.user.firstName, this.nameValidators),
+//     lastName: new FormControl(this.user.lastName, this.nameValidators),
+//     email: new FormControl(this.user.email, [Validators.required, Validators.email]),
+//     password: new FormControl(this.user.password, this.passwordValidators),
+//     confirmPassword: new FormControl(this.user.confirmPassword, [
+//       ...this.passwordValidators,
+//       matchPasswordValidator('password') // Add custom validator to check for password match
+//     ])
+//   });
+
+//   get firstName() {
+//     return this.myForm.get('firstName');
+//   }
+//   get lastName() {
+//     return this.myForm.get('lastName');
+//   }
+//   get email() {
+//     return this.myForm.get('email');
+//   }
+//   get password() {
+//     return this.myForm.get('password');
+//   }
+//   get confirmPassword() {
+//     return this.myForm.get('confirmPassword');
+//   }
+
+//   onSubmit() {
+//     console.log(this.myForm.value);
+//   }
+// }
